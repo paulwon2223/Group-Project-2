e5f0c54763e50dad481758d2213318600953951f
George Escobar <geo.escobar214@gmail.com>
2022-05-31 18:53:59 -0500

    landing page html/css

---
+++ script.js
@@ -0,0 +1,11 @@
+const requestUrl =
+  "https://newsdata.io/api/1/news?apikey=pub_7851a852526cac7c6cae4c1a0b0af3705169&category=technology";
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(data.results);
+  });
