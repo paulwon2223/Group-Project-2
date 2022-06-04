00a4c5a21fd9bb8ffddb939088231f326c0b882f
J Clancy <ukg_123@yahoo.com>
2022-06-04 11:44:58 -0500

    logout added

--- seeds/seed.js
+++ seeds/seed.js
@@ -5,38 +5,17 @@ const postData = require("./postData.json");
 const profileData = require("./profileData.json");
 const userData = require("./userData.json");
 
-// const seedDatabase = async() => {
-//     await sequelize.sync({ force: true });
+const seedDatabase = async() => {
+    await sequelize.sync({ force: true });
 
-//     await User.bulkCreate(userData, {
-//       individualHooks: true,
-//       returning: true,
-//       ignoreDuplicates: true,
-//     });
-//     // NOT SURE WHAT THIS DOES
-//     process.exit(0);
-// };
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+        ignoreDuplicates: true,
+    });
+    // NOT SURE WHAT THIS DOES
+    Post.bulkCreate(postData);
+    process.exit(0);
+};
 
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
-
-//   await Profile.bulkCreate(profileData, {
-//     individualHooks: true,
-//     returning: true,
-//   });
-//   // NOT SURE WHAT THIS DOES
-//   process.exit(0);
-// };
-
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
-
-//   await Post.bulkCreate(postData, {
-//     individualHooks: true,
-//     returning: true,
-//   });
-//   // NOT SURE WHAT THIS DOES
-//   process.exit(0);
-// };
-
-// seedDatabase();
+seedDatabase();
